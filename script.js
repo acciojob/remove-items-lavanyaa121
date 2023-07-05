10a1066fe9981311edbdcf7b86f6bb4be7a31380
lavanyaa121 <121863812+lavanyaa121@users.noreply.github.com>
2023-07-05 20:27:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 //your JS code here. If required.
-function func_remove(){
-   const elem = document.getElementById('colorSelect');
-   const pick =  elem.remove(elem.selectedIndex);
-   
+let select = document.getElementById("colorSelect");
+let btn =  document.querySelector("input[type=button]")
+btn.addEventListener("click", ()=>{       
+	//select.children--> will give all the child elements of select element
+    for (const child of select.children) {
+		//select.value--> will give value of the selected element
+        if(child.value === select.value){
+			//child.remove()-->remove the child element
+                child.remove();
+         }
+    }
+})
 }
